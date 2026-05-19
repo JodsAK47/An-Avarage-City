add21aaf0abdace4952fc378f3325d72b8a202c9
martinianodantas <martiniano.dantas@escolar.ifrn.edu.br>
2026-05-19 10:20:08 -0300

    Update game.py

--- Codigo Do Projeto/game.py
+++ Codigo Do Projeto/game.py
@@ -1,1 +1,25 @@
+import pygame
+import sys
+class Jogo:
 
+    def __init__(self):
+
+        pygame.init()
+
+        self.largura = 800
+        self.altura = 600
+
+        self.tela = pygame.display.set_mode(
+            (self.largura,self.altura)
+        )
+        self.rodador = True 
+        pygame.display.set_caption("An-Avarage-City")
+    def executar(self):
+        while self.rodando:
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    self.rodando = False
+            self.tela.fill((0, 0, 0))
+            pygame.display.flip()
+        pygame.quit()
+        sys.exit()
