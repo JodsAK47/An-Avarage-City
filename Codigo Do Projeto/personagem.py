c9d8e61aa2dc78c0299280cdb9bfcf708952cab4
JodsAK47 <jusdfran.m@escolar.ifrn.edu.br>
2026-05-19 10:12:52 -0300

    Add files via upload

---
+++ Codigo Do Projeto/personagem.py
@@ -0,0 +1,35 @@
+class personagem:
+
+    def __init__(self,x,y):
+        self.y = y
+        self.x = x
+        self.nome = "nome"
+        self.vida = 30
+        self.mpm = 6
+        self.cor = (255,0,255)
+        self.build = "nome do verso"
+        self.nivel = 1
+        self.atributo = { 
+            "Agi": 1 ,
+            "For": 1 ,
+            "Con": 1 ,
+            "Sab": 1 ,             
+                    }
+        
+
+class inimigo:
+
+    def __init__(self,x,y):
+        self.y = y
+        self.x = x
+        self.xp = 5
+        self.vida = 0
+        self.mpm = 6
+        self.cor = (255,0,255)
+        self.build = "nome do verso"
+        self.atributo = { 
+            "Agi": 1 ,
+            "For": 1 ,
+            "Con": 1 ,
+            "Sab": 1 ,             
+                    }
