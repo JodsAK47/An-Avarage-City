fed505bc84ffb392928a409fc8058c0e79b12d43
martinianodantas <martiniano.dantas@escolar.ifrn.edu.br>
2026-07-07 09:51:38 -0300

    Update Inimigo.py

---
+++ Codigo Do Projeto/Inimigo.py
@@ -0,0 +1,78 @@
+import pygame
+
+# ==========================
+# CLASSE INIMIGO
+# ==========================
+
+class Inimigo:
+
+    def __init__(self, nome, vida, forca, xp, x, y, cor):
+
+        self.nome = nome
+
+        self.vida_max = vida
+        self.vida = vida
+        self.xp = xp
+
+        self.x = x
+        self.y = y
+
+        self.cor = cor
+
+        self.atributos = {
+            "For": forca
+        }
+
+    def atacar(self):
+        return self.atributos["For"]
+
+    def receber_dano(self, dano):
+
+        self.vida -= dano
+
+        if self.vida < 0:
+            self.vida = 0
+
+    def vivo(self):
+        return self.vida > 0
+
+    def desenhar(self, tela):
+
+        pygame.draw.rect(
+            tela,
+            self.cor,
+            (self.x, self.y, 80, 80)
+        )
+
+
+
+inimigo1 = Inimigo(
+    nome="Inimigo 1",
+    vida=15,
+    forca=3,
+    xp=10,
+    x=550,
+    y=150,
+    cor=(0, 255, 0)
+)
+
+
+inimigo2 = Inimigo(
+    nome="Inimigo 2",
+    vida=25,
+    forca=5,
+    xp=20,
+    x=550,
+    y=250,
+    cor=(255, 255, 0)
+)
+
+chefe = Inimigo(
+    nome="Chefe",
+    vida=80,
+    forca=10,
+    xp=100,
+    x=550,
+    y=350,
+    cor=(255, 0, 0)
+)
